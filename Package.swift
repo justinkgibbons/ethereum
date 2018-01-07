1292e7fb99c11943b50772ac7e9dd153ce21f653
Justin Gibbons <justin@Justins-MacBook.local>
2018-01-07 12:22:53 -0800

    capitalize library name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "ethereum",
+    name: "Ethereum",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "ethereum",
-            targets: ["ethereum"]),
+            name: "Ethereum",
+            targets: ["Ethereum"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ethereum",
+            name: "Ethereum",
             dependencies: []),
         .testTarget(
-            name: "ethereumTests",
-            dependencies: ["ethereum"]),
+            name: "EthereumTests",
+            dependencies: ["Ethereum"]),
     ]
 )
