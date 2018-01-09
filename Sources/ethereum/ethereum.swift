fb068a1f8d2b8bf2867d1fd22f0d425d88f974dc
Justin Gibbons <justin@Justins-MacBook.local>
2018-01-08 20:27:55 -0800

    switch old methods over to new networkRequest

--- Sources/ethereum/ethereum.swift
+++ Sources/ethereum/ethereum.swift
@@ -7,6 +7,11 @@ public class Ethereum {
   public let rpcVersion = "2.0"
   public var requestId = 0
 
+  //MARK: Computed Properties
+  public var networkSession: URLSession {
+    return URLSession.shared
+  }
+
   //MARK: Initializer
   public init(using node: String) {
     self.node = node
@@ -23,7 +28,7 @@ public class Ethereum {
     return data
   }
 
-  public func xRequest(with json: Data) -> URLRequest {
+  public func networkRequest(with json: Data) -> URLRequest {
     let url = URL(string: self.node)!
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
@@ -33,39 +38,7 @@ public class Ethereum {
     return request
   }
 
-  public var networkSession: URLSession {
-    return URLSession.shared
-  }
-
-  public func networkRequest(with json: Data, dataRetrieved: @escaping (String) -> Void) {
-    //URL Request
-    let url = URL(string: self.node)!
-    let session = URLSession.shared
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    request.httpBody = json
-    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.addValue("application/json", forHTTPHeaderField: "Accept")
-
-    let requestFinished: (Data?, URLResponse?, Error?) -> Void  = { data, response, error in
-      if let data = data {
-        do {
-          let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-          let result = jsonSerialized["result"]!
-          dataRetrieved(result as! String) //make as! Any
-        } catch {
-          print("Network error: \(error)")
-        }
-      }
-    }
-
-    //Execute Task
-    let task = session.dataTask(with: request, completionHandler: requestFinished)
-
-    task.resume()
-    self.requestId += 1
-  }
-
+  //Validate JSON
   func validated(_ data: Data?) -> Any? {
     let result: Any?
     if let data = data {
@@ -83,19 +56,33 @@ public class Ethereum {
   }
 
   //Convenience Methods
-  public func balance(of wallet: String, callback: @escaping (String) -> Void) {
+  public func balance(of wallet: String, dataRetrieved: @escaping (String) -> Void) {
     let json = self.jsonData(method: "eth_getBalance", params: [wallet, "latest"])
-    self.networkRequest(with: json!, dataRetrieved: callback)
+    let request = self.networkRequest(with: json!)
+    let task = self.networkSession.dataTask(with: request) {
+      data, response, error in
+      if let data = self.validated(data) {
+        dataRetrieved(data as! String)
+      }
+    }
+    task.resume()
   }
 
-  public func newAccount(password: String, callback: @escaping (String) -> Void) {
+  public func newAccount(password: String, dataRetrieved: @escaping (String) -> Void) {
     let json = self.jsonData(method: "personal_newAccount", params: [password])
-    self.networkRequest(with: json!, dataRetrieved: callback)
+    let request = self.networkRequest(with: json!)
+    let task = self.networkSession.dataTask(with: request) {
+      data, response, error in
+      if let data = self.validated(data) {
+        dataRetrieved(data as! String)
+      }
+    }
+    task.resume()
   }
 
   public func accounts(dataRetrieved: @escaping ([String]) -> Void) {
     let json = self.jsonData(method: "personal_listAccounts", params: [])
-    let request = self.xRequest(with: json!)
+    let request = self.networkRequest(with: json!)
     let task = self.networkSession.dataTask(with: request) {
       data, response, error in
       if let data = self.validated(data) {
